1e6a150323ec585766726b8378858186807d75d0
cs.benliu <cs.benliu@gmail.com>
2025-02-23 15:45:06 -0500

    push q3

--- lab1.py
+++ lab1.py
@@ -1,7 +1,7 @@
 import math
 import random 
 
-random.seed(42)  # You can use any integer value as the seed
+#random.seed(42) 
 
 def run_simulation(num_games):
 
@@ -46,12 +46,48 @@ def play_round(prob): # i is the amount of money player 1 has, j is the amount o
     
 
 
+def run_simulation_q3(num_games): 
+    playerAwins = 0 
+    playerBwins = 0 
+    for i in range(num_games): 
+        if play_game_q3(): 
+            playerAwins += 1 
+        else: 
+            playerBwins += 1 
+
+    print(f"Player A wins {playerAwins} times")
+    print(f"Player B wins {playerBwins} times")
+    print(f"Probability that player A wins: {playerAwins/num_games}")
+
+def play_game_q3(): 
+    n = 200
+    i = n/2 
+    p = 0.49 
+    while(i > 0 and i < n): 
+        won = play_round_q3(p)
+        if won == 0: 
+            i += 1 
+        elif won == 1: 
+            i -= 1 
+        else: 
+            continue 
+    return i == n # if player A wins = true, if player B wins = false        
+def play_round_q3(p): 
+
+    r = random.random()
+    if r <= p: 
+        return 0  # Player A wins 
+    elif r > p and r <= 2*p: 
+        return 1  # Player B wins 
+    elif r > 2*p: 
+        return 2  # Tie 
+
 
 
 
 if __name__ == "__main__":
 
-    run_simulation(1000)
+    run_simulation_q3(10000)
 
 
 
