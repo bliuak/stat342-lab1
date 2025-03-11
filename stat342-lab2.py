b41096c78465413c6f252d9bf191b6c1eccb8813
bliuak <cs.benliu@gmail.com>
2025-03-11 15:58:39 -0400

    f

--- stat342-lab2.py
+++ stat342-lab2.py
@@ -32,17 +32,25 @@ def calculate_expected_time_to_win(prob_array):
 
 
 def calculate_expected_state7_visits(prob_array): 
+    # The expected number of visits to state 7 is given that we started in state 1 is given by 
+    # the 0, 3 entry of the matrix N=(I-Q)^-1
+    array = np.array(prob_array)
+    n_matrix = np.linalg.inv(np.eye(array.shape[0]) - array)
+    return n_matrix[0, 3]
+
+    
 def run_game(): 
     
     player_position = 0 # Initially in Box 1 
     rounds = 0 
-    print_state(player_position)  # Print initial state
+    num_times_in_state7 = 0
     while player_position != 4: 
         player_position = run_round(player_position)
         rounds += 1 
+        if player_position == 3: 
+            num_times_in_state7 += 1 
 
-    print(f"Player wins in {rounds} rounds")
-
+    return rounds, num_times_in_state7
 
 def print_state(player_position): 
     if player_position == 0: 
@@ -67,7 +75,6 @@ def run_round(player_position):
         rand -= array[pos][i]
         i+=1
     i -= 1 
-    print_state(i)
     return i
 
     
@@ -76,6 +83,16 @@ def run_round(player_position):
 
 if __name__ == "__main__":
 
-    print(calculate_expected_time_to_win(q_array))
-    run_game() 
+    print(f"The expected number of rounds to win is {round(calculate_expected_time_to_win(q_array)[0], 4)}")
+    print(f"The expected number of visits to state 7 is {round(calculate_expected_state7_visits(q_array), 4)}")
 
+    # Run the game 1000 times and print the average number of rounds and the average number of times in state 7
+    num_runs = 1000
+    total_rounds = 0
+    total_times_in_state7 = 0
+    for i in range(num_runs):
+        rounds, num_times_in_state7 = run_game()
+        total_rounds += rounds
+        total_times_in_state7 += num_times_in_state7
+    print(f"Average number of rounds: {total_rounds / num_runs}")
+    print(f"Average number of times in state 7: {total_times_in_state7 / num_runs}")
